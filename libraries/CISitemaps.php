21be7074f5172a5797eb12b75d40aced9d696522
Francis Ilechukwu <francis94c@gmail.com>
2019-03-24 13:52:39 +0100

    submit_sitemap and constructor.

--- libraries/CISitemaps.php
+++ libraries/CISitemaps.php
@@ -3,5 +3,26 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class CISitemaps {
 
+  private $base_url;
+
+  function __construct($params) {
+    if (isset($params["base_url"])) $this->base_url = $params["base_url"];
+    if ($base_url != null) $this->base_url = trim($this->base_url, "/") . "/";
+  }
+  /**
+   * [submit_sitemap description]
+   * @param  [type] $path [description]
+   * @return [type]       [description]
+   */
+  function submit_sitemap($path=null) {
+    if ($path == null) {
+      $sitemap_path = $this->base_url . "sitemap.xml";
+    } else {
+      $sitemap_path = $this->base_url == null ? $sitemap_path = $path : $this->base_url . $path;
+    }
+    $url = "http://www.google.com/webmasters/sitemaps/ping?sitemap=" . htmlentities($sitemap_path);
+    $response = file_get_contents($url);
+    return $response ? true : false;
+  }
 }
 ?>
