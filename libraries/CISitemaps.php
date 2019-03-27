aeeb8de8aae9987c2ed70023e5b2948c405303c8
Francis Ilechukwu <francis94c@gmail.com>
2019-03-27 21:18:45 +0100

    submit_sitemap() improvements.

--- libraries/CISitemaps.php
+++ libraries/CISitemaps.php
@@ -5,21 +5,23 @@ class CISitemaps {
 
   private $base_url;
 
+  private $ci;
+
   function __construct($params) {
     if (isset($params["base_url"])) $this->base_url = $params["base_url"];
     if ($base_url != null) $this->base_url = trim($this->base_url, "/") . "/";
+    $this->ci =& get_instance();
   }
   /**
    * [submit_sitemap description]
-   * @param  [type] $path [description]
-   * @return [type]       [description]
+   * @param  string $sitemap [description]
+   * @return [type]          [description]
    */
-  function submit_sitemap($path=null) {
-    if ($path == null) {
-      $sitemap_path = $this->base_url . "sitemap.xml";
-    } else {
-      $sitemap_path = $this->base_url == null ? $sitemap_path = $path : $this->base_url . $path;
+  function submit_sitemap($sitemap="sitemap.xml") {
+    if ($this->base_url == null) {
+      $this->ci->load->helper("url");
     }
+    $sitemap_path = $this->base_url == null ? base_url($sitemap) : $this->base_url . $sitemap;
     $url = "http://www.google.com/webmasters/sitemaps/ping?sitemap=" . htmlentities($sitemap_path);
     $response = file_get_contents($url);
     return $response ? true : false;
